623e3079357e7b6a03051f167c2cc803bb4066d5
johndavidmurff41 <john.murff562@students.cmcss.net>
2025-12-17 09:06:07 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -22,9 +22,9 @@ print(f'Each person gets : {share}')
 print(f'Leftover pizza : {leftover}')
 #Mom says "Wait, Brandon’s coming. We’re going to need more pizza. I’ll upgrade the mini to a party_pizza instead. It’s the same as 2 minis. Hopefully the leftovers will be enough to fill his hollow leg.”
 
-# TODO Update slices
-# TODO Recalculate share
-# TODO Recalculate leftover
-# TODO Print share result
-# TODO Print leftover result
+people += 1
+share = total_pizza // people
+leftover = total_pizza % people
+print(f'Each person gets : {share}')
+print(f'Leftover pizza : {leftover}')
 print("...for Mr. Hollow Leg")
