65e7f7fa472e47570181ffaeb0399bd19ad4c8a6
johndavidmurff41 <john.murff562@students.cmcss.net>
2025-12-17 08:58:26 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,25 @@
+#Do Not use crtl+A to select code.  Only copy the code below this line.
+#------------------------------------------------------------------------------------------------
+total_pizza = large + medium
+print(f'{total_pizza}')
 
+people += 1
+share = total_pizza // people
+leftover = total_pizza % people
+print(f'Each person gets : {share}')
+print(f'Leftover pizza : {leftover}')
+
+# TODO Update people #Eric and Brandon are coming too.
+# TODO Recalculate share
+# TODO Recalculate leftover
+# TODO Print share results
+# TODO Print leftover result
+
+#Mom says "Wait, Brandon’s coming. We’re going to need more pizza. I’ll upgrade the mini to a party_pizza instead. It’s the same as 2 minis. Hopefully the leftovers will be enough to fill his hollow leg.”
+
+# TODO Update slices
+# TODO Recalculate share
+# TODO Recalculate leftover
+# TODO Print share result
+# TODO Print leftover result
+print("...for Mr. Hollow Leg")
